42f881ec2077242a2974dda6cd75d62fb8deb555
unknown <richwbates@gmail.com>
2017-08-11 20:38:54 -0400

    added comments

--- assets/js/game.js
+++ assets/js/game.js
@@ -28,42 +28,61 @@ new CreateCharacter("CT-5555","assets/images/characters/ct5555.jpg",30, 4, 2,1);
 new CreateCharacter("General Grevious","assets/images/characters/grevious.jpg", 35, 2, 1, 4);
 new CreateCharacter("Count Doku","assets/images/characters/doku.jpg", 40, 10, 5, 1);
 
+//waiting select button to be clicked to choose player character
 $(".select").on("click", function(){
+	//gets data-character attribute to get characters name
 	var player = $(this).attr("data-character");
+	//takes player name and appends to target that characters attack button
 	var selected = "#" + player + "attack";
 
 
 	console.log(selected);
+	//switches attack button of player character from invisible to visible class
 	$(selected).removeClass("invisible");
 	$(selected).addClass("visible");
 
 	
 	// consider switch to individual classes for the buttons so that you can modify the css directly without having to add and remove classes
+	// hides the select button on all characters
 	characterLoop("#", "select", "visible" ,"remove");
 	characterLoop("#", "select", "invisible" ,"add",);
 
+	//calls selectOpponent function and passes player characters name
 	selectOpponent(player);
 
 });
 
+//function to select opponent
 function selectOpponent(playerChar){
 	characterLoop("#", "opponent", "visible" ,"add", playerChar);
 	characterLoop("#", "opponent", "invisible" ,"remove", playerChar);
 }
 
+// function that allows the processing of any information that involves majority of characters
+// selector is either "."" or "#" for classes or IDs 
+// selectorName is unique button name 
+// elementName is the class/id name can be blank if not working with a class or id (css change) 
+// action is the action to be taken must match switch
+// ignoreCharacter character to not apply a change to
 function characterLoop(selector, selectorName, elementName = "", action, ignoreCharacter = ""){
+	//loops through the characterName array
 	for (var i = 0; i < characterNames.length; i++) {
+		//if ignoreCharacters is set it will not make changes to that character (generally player's character)
 		if(characterNames[i] != ignoreCharacter){
+			//combine the variable to create a jquery selector comment for classes or ids
 			var element = selector + characterNames[i] + selectorName;
-
+			//switch statement to process the action 
 			switch(action){
 				case action = "add":
+					//adds a class
 					$(element).addClass(elementName);
 					break;
 				case action = "remove":
+					//removes a class
 					$(element).removeClass(elementName)
 					break;
 				case action = "css":
+					//modifies css
 					$(element).css("visibility", value);
 					break;
 				default:
